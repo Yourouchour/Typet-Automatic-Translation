34ae651d93adb1ff195fb7ec127ff027d1a64272
Yourouchour <137379846+Yourouchour@users.noreply.github.com>
2025-08-13 18:23:53 +0800

    Update typst-automatic-translation.user.js

--- typst-automatic-translation.user.js
+++ typst-automatic-translation.user.js
@@ -41,39 +41,66 @@
         "Tips": "提示",
         "Feedback": "反馈",
         "Support": "支持",
+        "Projects": "所有项目",
+        "Dashboard": "控制台",
+        "My Projects": "我的项目",
+        "Empty document": "空白文档",
+        "Start from scratch": "从头开始",
+        "Start from template": "使用模板",
+        "Configure a template to get going": "配置一个模板以开始",
     }
     const Translation = ChineseTranslation;
+    function translateFragment(fragment) {
+        const newText = Translation[fragment.innerText];
+        if (newText) {
+            fragment.innerText = newText;
+        }
+    }
     function translate(mutationList, observer) {
         console.log("Translating...");
         observer.disconnect();
         const headerButtons = document.querySelector("header")?.querySelectorAll("button");
         if (headerButtons) {
             headerButtons.forEach(button => {
-                const newText = Translation[button.innerText];
-                if (newText) {
-                    button.innerText = newText;
-                }
+                translateFragment(button);
             })
         }
         const menus = document.querySelectorAll("ul[role=menu]")
-        if (menus) {
-            menus.forEach(menu => {
-                const menuItems = menu.querySelectorAll("li[role=menuitem]")
-                if (menuItems) {
-                    menuItems.forEach(item => {
-                        const span = item.querySelector("span");
-                        if (span) {
-                            const newText = Translation[span.innerText];
-                            if (newText) {
-                                span.innerText = newText;
-                            }
-                        }
-                    })
-                }
+        menus.forEach(menu => {
+            const menuItems = menu.querySelectorAll("li[role=menuitem]")
+            if (menuItems) {
+                menuItems.forEach(item => {
+                    const span = item.querySelector("span");
+                    if (span) {
+                        translateFragment(span);
+                    }
+                })
+            }
+        })
+        const h1 = document.querySelector("h1");
+        if (h1) {
+            translateFragment(h1);
+        }
+        const h2 = document.querySelector("h2");
+        if (h2) {
+            translateFragment(h2);
+        }
+        const h3 = document.querySelector("h3");
+        if (h3) {
+            translateFragment(h3);
+        }
+        const projectListHeader = document.querySelector("div[class*=_projectListHeader]");
+        if (projectListHeader) {
+            const strongs = projectListHeader.querySelectorAll("strong");
+            strongs.forEach(strong => {
+                translateFragment(strong);
+            })
+            const spans = projectListHeader.querySelectorAll("span");
+            spans.forEach(span => {
+                translateFragment(span);
             })
         }
         observer.observe(document.body, { childList: true, subtree: true });
-
     }
     const observer = new MutationObserver(translate);
     observer.observe(document.body, { childList: true, subtree: true });
